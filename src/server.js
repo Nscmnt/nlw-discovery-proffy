2e74171a5b40bf265b6db238806bb540ba8a0dab
Jorge Nascimento <jorg3nascimento@gmail.com>
2020-08-13 12:19:26 -0300

    iniciando nunjucks

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const proffys = [
     {name: "Diego fernandes", 
     avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
     whatsapp:'11952454475',
-    bio: 'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas',
+    bio: 'Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas',
     subject: "Química",
     cost: "20",
     weekday: [0],
@@ -10,8 +10,8 @@ const proffys = [
     time_to:[1220],
 },
 
-{name: "Daniele", 
-    avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+{name: "Nascimento", 
+    avatar:"https://avatars3.githubusercontent.com/u/62806739?s=460&u=33d673930afa27361d44407d6e4937e9a626320f&v=4",
     whatsapp:'11952454475',
     bio: 'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas',
     subject: "Química",
@@ -20,25 +20,50 @@ const proffys = [
     time_from:[720],
     time_to:[1220],
 }
+]
 
+const subject = [
+'Artes',
+'Biologia',
+'Ciências',
+'Educação',
+'Física',
+'Geografia',
+'História',
+'Matemática',
+'Português',
+'Química'
 ]
 
 function pageLanding(req, res){
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 function pageStudy( req, res){
-    return res.sendFile(__dirname + "/views/study.html")
+    const filters = req.query;
+
+    return res.render("study.html", {proffys, filters})
 }
 
 function pageGiveClasses(req, res){
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    return res.render("give-classes.html")
 }
 
 const express = require('express')
 const server = express()
 
+// Configurar nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views',{
+    express: server,
+    noCache: true,
+
+})
+
+server
+// Configurar arquivos estaticos ( css, script, imagens)
 .use(express.static("public"))
+//Rotas da aplicação
 .get("/",pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
